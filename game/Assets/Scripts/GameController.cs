0bd7351525ef1dd647db439743e1dc062657ff11
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2023-08-16 13:24:07 -0700

    Cleaned up git binary assets

--- game/Assets/Scripts/GameController.cs
+++ game/Assets/Scripts/GameController.cs
@@ -4,41 +4,41 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject missilePrefab;
-    public GameObject[] cloudPrefabs;
+  public GameObject missilePrefab;
+  public GameObject[] cloudPrefabs;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  IEnumerator missileSpawner()
+  {
+    while (!gameOver)
     {
-
+      int j = 0;
+      int i = 0;
+      if (target.rotation.z < 180)
+      {
+        i = 10;
+        j = 8;
+      }
+      else
+      {
+        i = -10;
+        j = -8;
+      }
+      Vector3 spawnPosition = target.position + new Vector3(Random.Range(j, i), Random.Range(j, i), 0f);
+      GameObject missileTemp = Instantiate(missilePrefab, spawnPosition, missilePrefab.transform.rotation);
+      missileTemp.GetComponent<MissileBehaviour>().target = target;
+      yield return new WaitForSeconds(Random.Range(3f, 5f));
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    IEnumerator missileSpawner()
-    {
-        while (!gameOver)
-        {
-            int j = 0;
-            int i = 0;
-            if (target.rotation.z < 180)
-            {
-                i = 10;
-                j = 8;
-            }
-            else
-            {
-                i = -10;
-                j = -8;
-            }
-            Vector3 spawnPosition = target.position + new Vector3(Random.Range(j, i), Random.Range(j, i), 0f);
-            GameObject missileTemp = Instantiate(missilePrefab, spawnPosition, missilePrefab.transform.rotation);
-            missileTemp.GetComponent<MissileBehaviour>().target = target;
-            yield return new WaitForSeconds(Random.Range(3f, 5f));
-        }
-    }
+  }
 }
