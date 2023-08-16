1dc9f2b43ff17abb46e7e892b20eb1f79f819155
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2023-08-16 16:02:43 -0700

    [game] Re-enabled game end logic

--- game/Assets/Scripts/GameCtrl.cs
+++ game/Assets/Scripts/GameCtrl.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameCtrl : MonoBehaviour
 {
+  public Canvas endGameUi;
+
   private void OnPlayerExploded()
   {
     BroadcastMessage("OnGameEnd", SendMessageOptions.DontRequireReceiver);
+    this.endGameUi.gameObject.SetActive(true);
+  }
+
+  public void restart()
+  {
+    SceneManager.LoadScene("Main");
   }
 }
