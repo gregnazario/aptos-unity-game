1bdca86d05c9774b01d662aac67a024d1025e26b
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2023-08-16 14:15:21 -0700

    [game] Cleanup

--- game/Assets/Scripts/BackgroundController.cs
+++ game/Assets/Scripts/BackgroundController.cs
@@ -7,14 +7,22 @@ public class BackgroundConroller : MonoBehaviour
   public GameObject[] cloudPrefabs;
 
   // Start is called before the first frame update
-  void Start()
+  private void Start()
   {
-
+    makeClouds();
   }
 
-  // Update is called once per frame
-  void Update()
+  void makeClouds()
   {
-
+    for (int i = -40; i < 40; i += (int)Random.Range(5, 8))
+    {
+      for (int j = -40; j < 40; j += (int)Random.Range(5, 8))
+      {
+        var cloudPrefab = this.cloudPrefabs[Random.Range(0, cloudPrefabs.Length)];
+        var position = new Vector3(i, j, 0f);
+        var rotation = Quaternion.identity;
+        GameObject.Instantiate(cloudPrefab, position, rotation, this.transform);
+      }
+    }
   }
 }
